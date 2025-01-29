dd168357c1772a09d3f2dc3d2cc7e1cd35038d01
Tusharlohani <tusharlohani2@gmail.com>
2025-01-30 00:36:43 +0530

    Initial Commit

---
+++ src/main/java/com/dailyExpenseTracker/ExpenseTracker/Repository/ExpenseRepo.java
@@ -0,0 +1,6 @@
+package com.dailyExpenseTracker.ExpenseTracker.Repository;
+
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface ExpenseRepo<Expense> extends JpaRepository<Expense,Integer> {
+//}
